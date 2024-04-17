7d850dabc27be01080616f3f6d9100c2f9926fbb
Nitesh Kumar <imnksid@gmail.com>
2024-04-17 23:08:07 +0530

    Changed BoilerPlate to SpendingChart code

--- ios/SpendingChartTests/SpendingChartTests.m
+++ ios/SpendingChartTests/SpendingChartTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BoilerPlateTests : XCTestCase
+@interface SpendingChartTests : XCTestCase
 
 @end
 
-@implementation BoilerPlateTests
+@implementation SpendingChartTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
