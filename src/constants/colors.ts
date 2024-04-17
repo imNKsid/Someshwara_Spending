3eabe3ce32eb75d914c64b3beb70ac4d8528de75
Nitesh Kumar <imnksid@gmail.com>
2024-04-18 00:00:39 +0530

    Created colors file & added all colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  bgColor: "#F1F0F5",
+  black: "#1D1D2C",
+  green: "#35C937",
+  yellow: "#DABB4F",
+  darkBlue: "#5281AC",
+  orange: "#EE9E38",
+  blue: "#76A6D3",
+  magenta: "#DFA1A7",
+  lightBlue: "#5ACCD1",
+};
