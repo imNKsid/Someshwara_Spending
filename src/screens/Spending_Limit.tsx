d2547ae3957cd1397cd8ce47029a7f16f4ea209c
Nitesh Kumar <imnksid@gmail.com>
2024-04-17 23:59:56 +0530

    Created different sample screens

---
+++ src/screens/Spending_Limit.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SpendingLimit = () => {
+  return (
+    <View>
+      <Text>SpendingLimit</Text>
+    </View>
+  );
+};
+
+export default SpendingLimit;
+
+const styles = StyleSheet.create({});
