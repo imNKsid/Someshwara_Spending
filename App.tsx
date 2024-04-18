ac3148fb95597596f9107383f832358ec73c8510
Nitesh Kumar <imnksid@gmail.com>
2024-04-18 23:40:31 +0530

    Defined flex 1 to the App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import { Dashboard } from "./src/screens";
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Dashboard />
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
