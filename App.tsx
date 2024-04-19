a0777b1ce1f7a494875580e394b810f1c3a433c1
Nitesh Kumar <imnksid@gmail.com>
2024-04-20 02:01:02 +0530

    Redux store created & wrapped on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store/store";
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <Provider store={store}>
       <AppNavigator />
-    </View>
+    </Provider>
   );
 };
 
